35ec7b0211c6d18c1a1a64a9acad460d89524d22
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-09-17 17:00:14 +0700

    save

--- vite.config.ts
+++ vite.config.ts
@@ -11,19 +11,7 @@ export default defineConfig({
   define: {
     SERVER_URL: `http://localhost:8800/api/v1`
   },
-  resolve: {
-    alias: [
-      {find: '@api', replacement:'/src/api'},
-      {find: '@app', replacement:'/src/app'},
-      {find: '@assets', replacement:'/src/assets'},
-      {find: '@models', replacement:'/src/models'},
-      {find: '@components', replacement:'/src/components'},
-      {find: '@hooks', replacement:'/src/hooks'},
-      {find: '@pages', replacement:'/src/pages'},
-      {find: '@store', replacement:'/src/store'},
-      {find: '@styles', replacement:'/src/styles'}
-    ]
-  },
+
   esbuild: {
     jsxInject: `import React from 'react'`,
     treeShaking: true //команда, удаляющая неиспользуемый код
